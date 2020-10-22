01dc41567be510174eeacfb1b6126fcf36c44f06
MackRoe <elaine.music@students.makeschool.com>
2020-10-22 10:11:00 -0500

    first commit

---
+++ sorting_iterative.py
@@ -0,0 +1,43 @@
+#!python
+
+
+def is_sorted(items):
+    """Return a boolean indicating whether given items are in sorted order."""
+    for i in range(len(items)-1):
+        if items[i] <= items[i+1]:
+            continue
+    else:
+        return False
+    return True
+    # TODO: Running time: ??? Why and under what conditions? O(n)
+    # TODO: Memory usage: ??? Why and under what conditions? "Good"
+    # TODO: Check that all adjacent items are in order, return early if so
+
+
+def bubble_sort(items):
+    """Sort given items by swapping adjacent items that are out of order, and
+    repeating until all items are in sorted order.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Repeat until all items are in sorted order
+    # TODO: Swap adjacent items that are out of order
+
+
+def selection_sort(items):
+    """Sort given items by finding minimum item, swapping it with first
+    unsorted item, and repeating until all items are in sorted order.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Repeat until all items are in sorted order
+    # TODO: Find minimum item in unsorted items
+    # TODO: Swap it with first unsorted item
+
+
+def insertion_sort(items):
+    """Sort given items by taking first unsorted item, inserting it in sorted
+    order in front of items, and repeating until all items are in order.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Repeat until all items are in sorted order
+    # TODO: Take first unsorted item
+    # TODO: Insert it in sorted order in front of items
