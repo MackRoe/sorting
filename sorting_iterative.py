72f338e21eeb9ccdeea0c2e5ccf2e72680c434f8
MackRoe <elaine.music@students.makeschool.com>
2020-10-30 08:57:15 -0500

    is_sorted function passes all tests

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -3,11 +3,13 @@
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order."""
+    if items == [] or len(items) == 1:
+        return True
     for i in range(len(items)-1):
         if items[i] <= items[i+1]:
             continue
-    else:
-        return False
+        else:
+            return False
     return True
     # TODO: Running time: ??? Why and under what conditions? O(n)
     # TODO: Memory usage: ??? Why and under what conditions? "Good"
@@ -38,9 +40,25 @@ def selection_sort(items):
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    # start with nothing is_sorted and assume ascending order
+    unsorted_length = range(0, len(items) - 1)  # subtract one to account for last item
+    print('length of items array: ' + str(unsorted_length))
+    # count = 0
+    # while unsorted_length >= count:
+    # pass thru array find largest or >smallest
+    for i in unsorted_length:
+        min_item_location = i
+        for j in range(i + 1, len(items)):
+            print("i: ", i, "j: ", j)
+            if items[j] < items[i]:
+                min_item_location = j
+
+        if min_item_location != i:  # if location is not i, then swap is needed
+            # move found value to >beginning or end
+            items[i], items[min_item_location] = items[min_item_location], items[i]
+            # count += 1
+
+    return items
 
 
 def insertion_sort(items):
@@ -51,3 +69,19 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+    current_position = 1
+    current_value = my_list[0]
+
+    # get values from my_list  ## pulled from repl.it
+    for i in range(1, len(my_list)):
+        current_value = my_list[i]
+        while current_position < 0:
+            if my_list[i] <= my_list[i-1]:
+                my_list[i-1], my_list[i] = my_list[i], my_list[i-1]
+                current_position += 1
+        current_position -= 0
+    return my_list
+
+
+test_array = [17, 3, 7, 9, 3, 16, 15, 5, 6,	13,	7]
+print(selection_sort(test_array))
